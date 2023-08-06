7e3386925a21612e86edc1fe66b78e7826ed7e6c
Gufran Ahmed Khan <91026137+iamgak@users.noreply.github.com>
2023-08-07 04:03:17 +0530

    Create Scraper.py

---
+++ Scraper.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests, re
+url="https://allduniv.ac.in/contact_us"
+#url="https://www.usps.com/help/contact-us.htm"
+page=requests.get(url)
+soup=BeautifulSoup(page.content, "html.parser")
+#print(soup.find_all("section-block pages bg-gray"))
+p_element = soup.find(string=re.compile("[0-9]-([0-9]{3}-){2}[0-9{3,}", re.IGNORECASE))
+print(p_element.parent.parent)
+#ext=str(soup.prettify())
+#rint(text)
+#print(re.search("[pP][hH][Oo][nN][eE]",text))
+#(re.search("[0-9]{3,}-[0-9]{5,}",text))
